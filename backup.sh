f3729bed8e7fcaad54c91f5d598be6ca259648ee
david <david.tabarie@gmail.com>
2019-02-09 15:13:44 +0100

    idem

--- backup.sh
+++ backup.sh
@@ -1,11 +1,11 @@
 #! /bin/bash
 
-sudo -u www-data php /var/www/nextcloud/occ maintenance:mode --on
+sudo -u www-data php /var/www/html/nextcloud/occ maintenance:mode --on
 
-sudo rsync -avxziptgo /var/www/nextcloud/ root@192.168.33.201:/root/backups/nextcloud/nextcloud_`date +%Y-%m-%w`/
+sudo rsync -avxziptgo /var/www/html/nextcloud/ root@192.168.33.201:/root/backups/nextcloud/nextcloud_`date +%Y-%m-%w`/
 
 sudo mysqldump --single-transaction -u root nextcloud > /root/sqlbck/nextcloud-sqlbkp_`date +%Y-%m-%w`.bak
 
 sudo rsync -avxziptgo /root/sqlbck/ root@192.168.33.201:/root/backups/nextcloud/
 
-sudo -u www-data php /var/www/nextcloud/occ maintenance:mode --off
+sudo -u www-data php /var/www/html/nextcloud/occ maintenance:mode --off
