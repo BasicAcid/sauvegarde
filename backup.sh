f4131e9d8e2e64348d91868c590769a79b4e97d4
david <david.tabarie@gmail.com>
2019-02-09 15:11:08 +0100

    Adapt directories

--- backup.sh
+++ backup.sh
@@ -2,10 +2,10 @@
 
 sudo -u www-data php /var/www/nextcloud/occ maintenance:mode --on
 
-sudo rsync -avxziptgo /var/www/nextcloud/ david@poseidon:/home/david/HDD/backups/nextcloud/nextcloud_`date +%Y-%m-%w`/
+sudo rsync -avxziptgo /var/www/nextcloud/ root@192.168.33.201:/root/backups/nextcloud/nextcloud_`date +%Y-%m-%w`/
 
-sudo mysqldump --single-transaction  -u root nextcloud > /home/david/sqlbkp/nextcloud-sqlbkp_`date +%Y-%m-%w`.bak
+sudo mysqldump --single-transaction -u root nextcloud > /root/sqlbck/nextcloud-sqlbkp_`date +%Y-%m-%w`.bak
 
-sudo rsync -avxziptgo /home/david/sqlbkp david@poseidon:/home/david/HDD/backups/nextcloud/
+sudo rsync -avxziptgo /root/sqlbck/ root@192.168.33.201:/root/backups/nextcloud/
 
 sudo -u www-data php /var/www/nextcloud/occ maintenance:mode --off
