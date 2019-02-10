763e292394789cbedb07b0db0f8d97e4d59ecce9
david <david.tabarie@gmail.com>
2019-02-10 17:12:36 +0100

    update readme

--- README.md
+++ README.md
@@ -1,5 +1,23 @@
 # Sauvegarde Cloud
 
+## Description du travail à effectuer
+Vous devez mettre en place la sauvegarde de cette application de la
+manière la plus efficace possible en considérant que le volume de
+données peut devenir très important.  De plus, vous devrez permettre
+une historisation de ces sauvegardes avec une durée de rétention de 30
+jours.  Vous sauvegarderez bien évidemment les fichiers aussi bien que
+la base de données de manière à ce qu’une restauration soit
+parfaitement possible.  Vous planifierez cette sauvegarde de manière à
+ce qu’elle s’effectue de manière totalement automatique, le plus
+fréquemment possible et en mettent en application les bonnes pratiques
+étudiées en cours.
+
+## Travail à rendre
+Vous enverrez l’ensemble des scripts ainsi qu’une procédure simplifiée
+d’utilisation et de restauration de votre système par mail à l’adresse suivante:
+anthony@avalone-fr.com
+La date limite de remise est fixé au 22 février 2019.
+
 ## Notes concernant l'installation
 Le sujet du TP ne concernant les éventuelles modifications des
 machines virtuelles fournies, nous nous contenteront d'expliquer la
--- restore.sh
+++ restore.sh
@@ -2,28 +2,27 @@
 
 # Warning: this script first delete every data under the /nextcloud directory
 
-# Turn maintenance mode ON
-sudo -u www-data php /var/www/nextcloud/occ maintenance:mode --on
+sudo -u www-data php /var/www/html/nextcloud/occ maintenance:mode --on
 
-rsync -Aax david@trinity:/home/david/nextcloud-dirbkp/ david@nyxcloud:/var/www/nextcloud/
+rsync -Aavxziptgo backup:/root/backup david@nyxcloud:/var/www/nextcloud/
 
 # Drop previous database
-mysql -u root -p[password] -e "DROP DATABASE nextcloud"
+mysql -u root -proot -e "DROP DATABASE nextcloud"
 
 # Create new database
-mysql -u root -p[password] -e "CREATE DATABASE nextcloud CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci"
+mysql -u root -proot -e "CREATE DATABASE nextcloud CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci"
 
 # Import data from backup
-mysql -h [server] -u [username] -p[password] [db_name] < nextcloud-sqlbkp.bak
+mysql -u root -proot nextcloud < nextcloud-sqlbkp.bak
 
 # Turn maintenance mode OFF
-sudo -u www-data php /var/www/nextcloud/occ maintenance:mode --off
+sudo -u www-data php /var/www/html/nextcloud/occ maintenance:mode --off
 
 # update the systems data-fingerprint after a backup is restored
-sudo -u www-data php /var/www/nextcloud/occ maintenance:data-fingerprint
+sudo -u www-data php /var/www/html/nextcloud/occ maintenance:data-fingerprint
 
 # rescan files
-sudo -u www-data php /var/www/nextcloud/occ files:scan --all
+sudo -u www-data php /var/www/html/nextcloud/occ files:scan --all
 
 # cleanup files
-sudo -u www-data php /var/www/nextcloud/occ files:cleanup
+sudo -u www-data php /var/www/html/nextcloud/occ files:cleanup
