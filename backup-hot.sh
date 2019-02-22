c2bb67bcd14fda8fbfbf6f506c3beb25b79c71cc
david <david.tabarie@gmail.com>
2019-02-22 23:42:25 +0100

    Repush backup-hot

--- backup-hot.sh
+++ backup-hot.sh
@@ -5,9 +5,6 @@
 NEXTCLOUD_DIR="/var/www/html/nextcloud"
 BCK_HOST="backup"
 
-# Turn maintenance mode ON
-sudo -u www-data php $NEXTCLOUD_DIR/occ maintenance:mode --on
-
 # Syncronize with BCK_HOST server, preserving labels and setting a dierctory name based on the current week
 sudo rsync -Aavxziptgo $NEXTCLOUD_DIR/ $BCK_HOST:/root/nextcloud_`date +%Y-%m-%W`/
 
@@ -19,6 +16,3 @@ sudo rsync -Aavxziptgo /root/*.bak $BCK_HOST:/root/nextcloud_`date +%Y-%m-%W`/
 
 # Delete the local dump
 rm /root/*.bak
-
-# Turn maintenance mode OFF
-sudo -u www-data php $NEXTCLOUD_DIR/occ maintenance:mode --off
